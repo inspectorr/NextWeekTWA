b3850e5e8cab952f7dde99010988078cc3f3bdd9
Roman Verkhoturov <r.verkhoturov@corp.mail.ru>
2023-10-02 04:27:40 +0300

    env naming

--- .dev/tunnel/index.js
+++ .dev/tunnel/index.js
@@ -4,7 +4,9 @@ const { createHash } = require('crypto');
 const PORT = 8765;
 
 function getSubdomain() {
-    const hash = createHash('sha256').update(process.env.TELEGRAM_BOT_TOKEN + process.env.APP_SALT).digest('hex');
+    const hash = createHash('sha256').update(
+        process.env.TELEGRAM_BOT_TOKEN + process.env.DEV_TUNNEL_SALT
+    ).digest('hex');
     return hash.slice(0, 10);
 }
 
